ced0e480368100940c187395d4fce3db06e14dca
Christinekimani <christinekimani881@gmail.com>
2024-10-11 09:24:56 +0300

    Initial commit

---
+++ kicks/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    email = models.EmailField(max_length=50)
+   
+    def __str__(self):
+        return f"The username is {self.username}  email is {self.email} "
